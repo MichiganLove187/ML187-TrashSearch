8697b0136683b1a4a5cbfb9cb5fbda602a44ff46
MichiganLove187 <147991628+MichiganLove187@users.noreply.github.com>
2025-05-23 18:45:39 -0500

    Update config.lua
    UPDATED PROPS THAT ARE SEARCHABLE

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 -- Cooldown between searches (in seconds)
-Config.Cooldown = 60
+Config.Cooldown = 120
 
 -- Chance to find something (0-100)
 Config.FindChance = 65
@@ -13,35 +13,52 @@ Config.MaxItems = 1
 Config.TrashObjects = {
     -- Trash Cans
     [GetHashKey("prop_bin_01a")] = true,
-    [GetHashKey("prop_bin_03a")] = true,
+    [GetHashKey("prop_bin_02a")] = true,
     [GetHashKey("prop_bin_05a")] = true,
     [GetHashKey("prop_bin_07a")] = true,
+    [GetHashKey("prop_bin_07b")] = true,
     [GetHashKey("prop_bin_08a")] = true,
     [GetHashKey("prop_bin_08open")] = true,
     [GetHashKey("prop_bin_09a")] = true,
+    [GetHashKey("prop_bin_07c")] = true,
+    [GetHashKey("prop_bin_06a")] = true,
+    [GetHashKey("prop_barrel_03d")] = true,
     
     -- Dumpsters
-    [GetHashKey("prop_dumpster_01a")] = false,
-    [GetHashKey("prop_dumpster_02a")] = false,
-    [GetHashKey("prop_dumpster_02b")] = false,
-    [GetHashKey("prop_dumpster_3a")] = false,
-    [GetHashKey("prop_dumpster_4a")] = false,
-    [GetHashKey("prop_dumpster_4b")] = false,
+    [GetHashKey("prop_dumpster_01a")] = true,
+    [GetHashKey("prop_dumpster_02a")] = true,
+    [GetHashKey("prop_dumpster_02b")] = true,
+    [GetHashKey("prop_dumpster_3a")] = true,
+    [GetHashKey("prop_dumpster_4a")] = true,
+    [GetHashKey("prop_dumpster_4b")] = true,
     
-    -- Trash Bags
-    [GetHashKey("prop_garbage_bag_01")] = true,
-    [GetHashKey("prop_cs_street_binbag_01")] = true
+    -- Trash Bags-- Trash Boxes--trash tires
+    [GetHashKey("prop_rub_binbag_01b")] = true,
+    [GetHashKey("prop_rub_binbag_03b")] = true,
+    [GetHashKey("prop_rub_cardpile_07")] = true,
+    [GetHashKey("prop_rub_boxpile_09")] = true,
+    [GetHashKey("prop_rub_boxpile_02")] = true,
+    [GetHashKey("prop_rub_pile_03")] = true,
+    [GetHashKey("prop_cratepile_05a")] = true,
+    [GetHashKey("prop_rub_pile_04")] = true,
+    [GetHashKey("prop_rub_binbag_05")] = true,
+    [GetHashKey("prop_rub_binbag_06")] = true,
+    [GetHashKey("prop_rub_binbag_03")] = true,
+    [GetHashKey("prop_rub_binbag_04")] = true,
+    [GetHashKey("prop_rub_boxpile_08")] = true,
+    [GetHashKey("prop_rub_boxpile_01")] = true,
+    [GetHashKey("prop_skid_trolley_1")] = true
 }
 
 -- Items that can be found in trash with their respective chances (out of 100)
 Config.TrashItems = {
     -- Common Items (Higher Chance)
     {item = "rubber", chance = 30, min = 1, max = 3},
-    {item = "rubber", chance = 30, min = 1, max = 3},
-    {item = "rubber", chance = 25, min = 1, max = 2},
-    {item = "rubber", chance = 25, min = 1, max = 3},
-    {item = "rubber", chance = 20, min = 1, max = 2},
-    {item = "rubber", chance = 20, min = 1, max = 2},
+    {item = "diamond_ring", chance = 30, min = 1, max = 3},
+    {item = "plastic", chance = 25, min = 1, max = 2},
+    {item = "copper", chance = 25, min = 1, max = 3},
+    {item = "electronics", chance = 20, min = 1, max = 2},
+    {item = "steel", chance = 20, min = 1, max = 2},
     
     -- Uncommon Items (Medium Chance)
     {item = "aluminum", chance = 15, min = 1, max = 2},
